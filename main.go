e23959c09a947f03ea6ec356e80dc0006fe534f6
alligator <enemy.forest.brigade@gmail.com>
2021-11-30 18:20:25 +0000

    add command line args

--- main.go
+++ main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	lang "github.com/alligator/advent-of-code-2021-lang/src"
 )
 
 func main() {
-	f, err := os.ReadFile("test.aoc")
+	dbgLex := flag.Bool("debug-lex", false, "debug lexing")
+	dbgAst := flag.Bool("debug-ast", false, "debug ast parsing")
+	flag.Parse()
+
+	filePath := flag.Arg(0)
+	if filePath == "" {
+		fmt.Fprintln(os.Stderr, "no file given!")
+		return
+	}
+
+	f, err := os.ReadFile(filePath)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		return
 	}
 
 	l := lang.NewLexer(string(f))
+
+	if *dbgLex {
+		for {
+			t := l.NextToken()
+			fmt.Printf("%v\n", t)
+			if t.Tag == lang.EOF {
+				break
+			}
+		}
+		return
+	}
+
 	p := lang.NewParser(&l)
 	prog := p.Parse()
-	// lang.PrettyPrint(&prog)
-	lang.Eval(&prog)
 
-	// for {
-	// 	t := l.NextToken()
-	// 	fmt.Printf("%v\n", t)
-	// 	if t.Tag == lang.EOF {
-	// 		break
-	// 	}
-	// }
+	if *dbgAst {
+		lang.PrettyPrint(&prog)
+		return
+	}
+
+	lang.Eval(&prog)
 }
